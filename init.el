0ab9420b6c8ea6112e1d159384fecfa4e2f53156
Bastian Bechtold <basti@bbheim.de>
2012-12-12 09:52:51 +0100

    Add shortcuts for German quotation marks

--- init.el
+++ init.el
@@ -122,6 +122,8 @@
 (global-set-key (kbd "M-/") "\\")
 (global-set-key (kbd "M-8") "{")
 (global-set-key (kbd "M-9") "}")
+(global-set-key (kbd "M-2") "„")
+(global-set-key (kbd "M-\"") "“")
 
 ;; Make command history persistent
 (savehist-mode t)
