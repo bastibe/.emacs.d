c8aa62192df436bd5b009df762b2ab681709bb7d
Bastian Bechtold <basti@bastibe.de>
2013-11-14 10:07:11 +0100

    fix regex search for org-agenda entries to correctly find org-journal files

--- init.el
+++ init.el
@@ -682,8 +682,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
- '(org-agenda-file-regexp "\\`[^.].*\\.org\\'\\|[0-9]+")
+ '(org-agenda-file-regexp "\\`[^.].*\\.org\\'\\|[0-9]+$")
  '(safe-local-variable-values (quote ((org-startup-folded "content") (org-set-startup-cisibility (quote content)) (backup-inhibited . t))))
+ '(send-mail-function (quote mailclient-send-it))
  '(sentence-end-double-space nil))
 
 (custom-set-faces
